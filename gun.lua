beca98af055821623d1c28c419b90d1407fd3177
Jasn <jasn74939@gmail.com>
2025-03-12 20:08:08 -0400

    Update gun.lua

--- gun.lua
+++ gun.lua
@@ -1,38 +1,56 @@
-
 local tool = script.Parent
-local handle = tool:WaitForChild("Handle")  
+local handle = tool:WaitForChild("Handle")
 local player = game.Players.LocalPlayer
-local mouse = player:GetMouse()  
+local mouse = player:GetMouse()
 
-local bulletSpeed = 100  
-local bulletSize = Vector3.new(0.2, 0.2, 1)  
-local reloadTime = 0.5  
+local bulletSpeed = 100
+local bulletSize = Vector3.new(0.2, 0.2, 1)
+local reloadTime = 0.5
 
-local canShoot = true 
+local canShoot = true
+
+
+local function onBulletTouch(hit)
+	
+	if hit:IsDescendantOf(player.Character) or hit == bullet then return end
+
+	
+	bullet:Destroy()
+
+	if hit and hit.Parent:FindFirstChild("Humanoid") then
+		local humanoid = hit.Parent:FindFirstChild("Humanoid")
+		if humanoid then
+			
+			humanoid:TakeDamage(10)  
+		end
+	end
+end
 
 local function shootBullet()
-    if not canShoot then return end 
+	if not canShoot then return end
 
-    canShoot = false  
-    
-    local bullet = Instance.new("Part")
-    bullet.Size = bulletSize
-    bullet.Position = handle.Position  
-    bullet.Anchored = false
-    bullet.CanCollide = true
-    bullet.Shape = Enum.PartType.Cylinder
-    bullet.BrickColor = BrickColor.new("Bright red")
-    bullet.Parent = game.Workspace
+	canShoot = false
 
-    local bodyVelocity = Instance.new("BodyVelocity")
-    bodyVelocity.MaxForce = Vector3.new(100000, 100000, 100000) 
-    bodyVelocity.Velocity = (mouse.Hit.p - handle.Position).unit * bulletSpeed  
-    bodyVelocity.Parent = bullet
+	local bullet = Instance.new("Part")
+	bullet.Size = bulletSize
+	bullet.Position = handle.Position
+	bullet.Anchored = false
+	bullet.CanCollide = true
+	bullet.Shape = Enum.PartType.Cylinder
+	bullet.BrickColor = BrickColor.new("Bright red")
+	bullet.Parent = game.Workspace
 
-    game.Debris:AddItem(bullet, 5)
+	bullet.Touched:Connect(onBulletTouch)
 
-     wait(reloadTime)
-    canShoot = true
+	local bodyVelocity = Instance.new("BodyVelocity")
+	bodyVelocity.MaxForce = Vector3.new(100000, 100000, 100000)
+	bodyVelocity.Velocity = (mouse.Hit.p - handle.Position).unit * bulletSpeed
+	bodyVelocity.Parent = bullet
+
+	game.Debris:AddItem(bullet, 5)
+
+	wait(reloadTime)
+	canShoot = true
 end
 
 tool.Activated:Connect(shootBullet)
