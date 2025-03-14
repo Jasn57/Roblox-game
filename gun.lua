26056cba016f9611cfa8ab6df896e4a8b8c6e7d2
Jasn <jasn74939@gmail.com>
2025-03-14 18:48:50 -0400

    Update gun.lua

--- gun.lua
+++ gun.lua
@@ -6,51 +6,42 @@ local mouse = player:GetMouse()
 local bulletSpeed = 100
 local bulletSize = Vector3.new(0.2, 0.2, 1)
 local reloadTime = 0.5
-
 local canShoot = true
 
-
 local function onBulletTouch(hit)
-	
-	if hit:IsDescendantOf(player.Character) or hit == bullet then return end
+    if hit:IsDescendantOf(player.Character) or hit:IsA("Part") then return end
 
-	
-	bullet:Destroy()
+    if hit.Parent:FindFirstChild("Humanoid") then
+        hit.Parent.Humanoid:TakeDamage(10)
+    end
 
-	if hit and hit.Parent:FindFirstChild("Humanoid") then
-		local humanoid = hit.Parent:FindFirstChild("Humanoid")
-		if humanoid then
-			
-			humanoid:TakeDamage(10)  
-		end
-	end
+    hit:Destroy()
 end
 
 local function shootBullet()
-	if not canShoot then return end
+    if not canShoot then return end
+    canShoot = false
 
-	canShoot = false
+    local bullet = Instance.new("Part")
+    bullet.Size = bulletSize
+    bullet.Position = handle.Position
+    bullet.Shape = Enum.PartType.Cylinder
+    bullet.BrickColor = BrickColor.new("Bright red")
+    bullet.Anchored = false
+    bullet.CanCollide = true
+    bullet.Parent = game.Workspace
 
-	local bullet = Instance.new("Part")
-	bullet.Size = bulletSize
-	bullet.Position = handle.Position
-	bullet.Anchored = false
-	bullet.CanCollide = true
-	bullet.Shape = Enum.PartType.Cylinder
-	bullet.BrickColor = BrickColor.new("Bright red")
-	bullet.Parent = game.Workspace
+    bullet.Touched:Connect(onBulletTouch)
 
-	bullet.Touched:Connect(onBulletTouch)
+    local bodyVelocity = Instance.new("BodyVelocity")
+    bodyVelocity.MaxForce = Vector3.new(100000, 100000, 100000)
+    bodyVelocity.Velocity = (mouse.Hit.p - handle.Position).unit * bulletSpeed
+    bodyVelocity.Parent = bullet
 
-	local bodyVelocity = Instance.new("BodyVelocity")
-	bodyVelocity.MaxForce = Vector3.new(100000, 100000, 100000)
-	bodyVelocity.Velocity = (mouse.Hit.p - handle.Position).unit * bulletSpeed
-	bodyVelocity.Parent = bullet
+    game.Debris:AddItem(bullet, 5)
 
-	game.Debris:AddItem(bullet, 5)
-
-	wait(reloadTime)
-	canShoot = true
+    wait(reloadTime)
+    canShoot = true
 end
 
 tool.Activated:Connect(shootBullet)
