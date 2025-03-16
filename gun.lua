a05f4c38bc6a0e906d7d4d4124150d23ba27254d
Jasn <jasn74939@gmail.com>
2025-03-16 10:11:57 -0400

    Update gun.lua

--- gun.lua
+++ gun.lua
@@ -1,47 +1,27 @@
-local tool = script.Parent
-local handle = tool:WaitForChild("Handle")
-local player = game.Players.LocalPlayer
-local mouse = player:GetMouse()
-
+local gun = script.Parent
 local bulletSpeed = 100
-local bulletSize = Vector3.new(0.2, 0.2, 1)
-local reloadTime = 0.5
-local canShoot = true
+local damage = 25
 
-local function onBulletTouch(hit)
-    if hit:IsDescendantOf(player.Character) or hit:IsA("Part") then return end
-
-    if hit.Parent:FindFirstChild("Humanoid") then
-        hit.Parent.Humanoid:TakeDamage(10)
-    end
-
-    hit:Destroy()
-end
-
-local function shootBullet()
-    if not canShoot then return end
-    canShoot = false
-
+local function onActivated()
+    local player = game.Players:GetPlayerFromCharacter(gun.Parent)
+    local mouse = player:GetMouse()
+    
     local bullet = Instance.new("Part")
-    bullet.Size = bulletSize
-    bullet.Position = handle.Position
-    bullet.Shape = Enum.PartType.Cylinder
-    bullet.BrickColor = BrickColor.new("Bright red")
-    bullet.Anchored = false
-    bullet.CanCollide = true
-    bullet.Parent = game.Workspace
-
-    bullet.Touched:Connect(onBulletTouch)
-
-    local bodyVelocity = Instance.new("BodyVelocity")
-    bodyVelocity.MaxForce = Vector3.new(100000, 100000, 100000)
-    bodyVelocity.Velocity = (mouse.Hit.p - handle.Position).unit * bulletSpeed
-    bodyVelocity.Parent = bullet
-
-    game.Debris:AddItem(bullet, 5)
-
-    wait(reloadTime)
-    canShoot = true
+    bullet.Size = Vector3.new(0.2, 0.2, 0.2)
+    bullet.Shape = Enum.PartType.Ball
+    bullet.Material = Enum.Material.Neon
+    bullet.BrickColor = BrickColor.new("Bright yellow")
+    bullet.CFrame = gun.CFrame
+    bullet.Velocity = (mouse.Hit.p - gun.Position).unit * bulletSpeed
+    bullet.CanCollide = false
+    bullet.Parent = workspace
+    
+    bullet.Touched:Connect(function(hit)
+        if hit.Parent:FindFirstChild("Humanoid") then
+            hit.Parent.Humanoid:TakeDamage(damage)
+            bullet:Destroy()
+        end
+    end)
 end
 
-tool.Activated:Connect(shootBullet)
+gun.Activated:Connect(onActivated)
